b4b23bd6af8443c90037f2a1e7305933a8f9014f
Manojs018 <>
2026-03-05 21:14:04 +0530

    Initial commit

--- mini-keep/backend/models/Note.js
+++ mini-keep/backend/models/Note.js
@@ -31,6 +31,10 @@ const noteSchema = mongoose.Schema(
             type: Boolean,
             default: false,
         },
+        labels: {
+            type: [String],
+            default: [],
+        },
     },
     {
         timestamps: true,
