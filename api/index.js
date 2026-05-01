6c5f33025232f475584609b16547ad3e167f4241
Manojs018 <>
2026-05-01 16:09:38 +0530

    initial commit

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ const app = express();
 const allowedOrigins = [
     process.env.FRONTEND_URL,
     'https://mini-keep.vercel.app',
+    'https://mini-keepfor.vercel.app',
     'http://localhost:5000',
     'http://localhost:5173',
     'http://127.0.0.1:5000',
